5222e108ee6be1c438fb5a29dfc2af99665a9986
Tim Adigwe <Timothyadigwe2019@gmail.com>
2022-09-20 03:08:22 +0100

    sendSol transaction implemented

--- components/SendSolForm.tsx
+++ components/SendSolForm.tsx
@@ -1,12 +1,35 @@
 import { FC } from 'react'
 import styles from '../styles/Home.module.css'
-
+import { useConnection, useWallet } from '@solana/wallet-adapter-react'
+import * as Web3 from '@solana/web3.js'
 
 export const SendSolForm: FC = () => {
 
+ const { connection } = useConnection();
+ const { publicKey, sendTransaction } = useWallet();   
+
+     
+
     const sendSol = event => {
         event.preventDefault()
-        console.log(`Send ${event.target.amount.value} SOL to ${event.target.recipient.value}`)
+        //console.log(`Send ${event.target.amount.value} SOL to ${event.target.recipient.value}`)
+        if(!connection || !publicKey ) {
+            alert('Please connect your wallet ')
+            return
+        }
+        
+        const transaction = new Web3.Transaction()
+        const sendSolInstruction =  Web3.SystemProgram.transfer(
+            {
+            fromPubkey: publicKey,
+            toPubkey:  new Web3.PublicKey(event.target.recipient.value),
+            lamports: event.target.amount.value
+        }
+        )
+        transaction.add(sendSolInstruction)
+        sendTransaction(transaction, connection).then(sig => {
+            console.log(`You can view your transaction on the solana explorer at: \nhttps://explorer.solana.com/tx/${sig}?cluster=devnet`)
+        })
     }
 
     return (
